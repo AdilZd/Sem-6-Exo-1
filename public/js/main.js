3cc2aed0f38873eb889d386cee0a0cf6faf5179f
AdilZd <nowayat0me@gmail.com>
2023-01-03 11:01:42 +0100

    Fin exo

---
+++ public/js/main.js
@@ -0,0 +1,83 @@
+// 1 //
+
+// function add(x, y) {
+//     // Calculer la somme des deux nombres
+//     return x + y;
+
+//     // Afficher le résultat dans la console
+//     console.log(add);
+// }
+// let demo = add(3, 5);
+// console.log(demo);
+
+// 2 //
+
+// function sous(x, y) {
+//     // Calculer la somme des deux nombres
+//     return x - y;
+
+//     // Afficher le résultat dans la console
+//     console.log(sous);
+// }
+// let demo = add(3, 6);
+// console.log(demo);
+
+// 3 //
+
+// function mult(x, y) {
+//     // Calculer la somme des deux nombres
+//     return x * y;
+
+//     // Afficher le résultat dans la console
+//     console.log(sous);
+// }
+// let demo = mult(3, 5);
+// console.log(demo);
+
+// 4 // 
+
+// function div(x, y) {
+//     // Calculer la somme des deux nombres
+//     return x / y;
+
+//     // Afficher le résultat dans la console
+//     console.log(sous);
+// }
+// let demo = div(3, 5);
+// console.log(demo);
+
+// 5 //
+
+// function mod(x, y) {
+//     // Calculer la somme des deux nombres
+//     return x % y;
+
+//     // Afficher le résultat dans la console
+//     console.log(sous);
+// }
+// let demo = mod(3, 5);
+// console.log(demo);
+
+// 6 //
+
+// function carre(x) {
+//     return x * x;
+// }
+// let demo = carre(3);
+// console.log(demo);
+
+// 7 //
+
+// const exposant = (x, y) => {
+//     return Math.pow(x, y);
+// }
+// let demo = exposant(3, 5)
+// console.log(demo);
+
+// 8 //
+
+// function capitalize() {}
+// const str = 'malheureusement';
+// const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+// console.log(str2);
+
